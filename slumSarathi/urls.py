ccddab0246de21039f2dd4f8043ebb0e4a03f9a2
prashanna dhami <prashannadhami14@gmail.com>
2026-09-20 17:12:17 +0545

    Revert "add run_migrations route"
    This reverts commit 1f0fa54dbc4da4f099af6c3fb15039aad68f2b73.

--- slumSarathi/urls.py
+++ slumSarathi/urls.py
@@ -9,16 +9,10 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 
-try:
-    from workers import env as workers_env
-except Exception:
-    workers_env = None
-
 
 def run_migrations_view(request):
     token = request.GET.get('token')
-    expected = os.environ.get('MIGRATE_TOKEN') or getattr(workers_env, 'MIGRATE_TOKEN', 'change-me-please')
-    if token != expected:
+    if token != os.environ.get('MIGRATE_TOKEN', 'change-me-please'):
         return HttpResponseForbidden("Forbidden")
     out = io.StringIO()
     try:
