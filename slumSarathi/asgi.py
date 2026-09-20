54c3772c6e9399159f1d86f5e6a0e74514159716
prashanna dhami <prashannadhami14@gmail.com>
2026-09-20 16:25:29 +0545

    Revert "switch from wsgi to asgi"
    This reverts commit 025a18183939f1e361006c77131b9cd2b21faa8b.

--- slumSarathi/asgi.py
+++ slumSarathi/asgi.py
@@ -1,21 +1,16 @@
+"""
+ASGI config for slumSarathi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
 import os
-import sys
-
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
-
-if str(BASE_DIR) not in sys.path:
-    sys.path.insert(0, str(BASE_DIR))
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 from django.core.asgi import get_asgi_application
 
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
-try:
-    from workers.asgi import entrypoint
-    Default = entrypoint(application)
-except Exception:
-    Default = application
+application = get_asgi_application()
